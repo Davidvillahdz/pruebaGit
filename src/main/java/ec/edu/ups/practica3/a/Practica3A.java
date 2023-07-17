5263ab1209f90944f35666a2ef192368583ac429
David Villa <d01072004@gmail.com>
2023-07-17 09:39:00 -0500

    first commit

---
+++ src/main/java/ec/edu/ups/practica3/a/Practica3A.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ups.practica3.a;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Practica3A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
